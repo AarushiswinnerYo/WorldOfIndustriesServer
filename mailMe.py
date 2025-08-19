fa0f36fe2429af9d4cf8c84b2cec09f231579a78
AarushiswinnerYo <156099106+AarushiswinnerYo@users.noreply.github.com>
2025-08-19 18:27:54 +0530

    Y

--- mailMe.py
+++ mailMe.py
@@ -81,8 +81,8 @@ def main():
                 sender = os.getenv("EMAIL")
                 recipients = [senderAdd]
                 password = os.getenv("EM_PASS")
-                username = f.encrypt(username.encode('utf-8'))
-                senderAdd = f.encrypt(senderAdd.encode('utf-8'))
+                username = f.encrypt(username.encode('utf-8')).decode()
+                senderAdd = f.encrypt(senderAdd.encode('utf-8')).decode()
                 c={f"{username}":"{senderAdd}"}
                 names.insert_one(c)
                 send_email(subject, body, sender, recipients, password, "reg")
