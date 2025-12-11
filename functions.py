cc91446c133289a5142202136bd8137d0d455758
AarushiswinnerYo <156099106+AarushiswinnerYo@users.noreply.github.com>
2025-12-11 16:27:02 +0530

    tgfgy

--- functions.py
+++ functions.py
@@ -8,7 +8,7 @@ from email.mime.text import MIMEText
 port_number =1234
 msg = MIMEMultipart()
 
-cluster="mongodb://resources-lookup.gl.at.ply.gg:33966/?directConnection=true"
+cluster="mongodb://192.168.29.178:27017/?directConnection=true"
 client=MongoClient(cluster)
 db=client.Users
 profs=db.names
